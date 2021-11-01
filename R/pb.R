141d4e06a6d9685a611fce62f6404bf65d65648d
kearney <mike.wayne.kearney@gmail.com>
2021-11-01 11:16:45 -0500

    imrove progress bar

--- R/pb.R
+++ R/pb.R
@@ -3,7 +3,7 @@ pblue <- function(x) {
     paste0("\033[38;5;110m", x, "\033[39m")
 }
 pmint <- function(x) {
-    paste0("\033[38;5;85m", x, "\033[39m")
+    paste0("\033[38;5;36m", x, "\033[39m")
 }
 ppink <- function(x) {
     paste0("\033[38;5;198m", x, "\033[39m")
@@ -11,19 +11,47 @@ ppink <- function(x) {
 ppurp <- function(x) {
     paste0("\033[38;5;99m", x, "\033[39m")
 }
+pgray <- function(x) {
+    paste0("\033[38;5;241m", x, "\033[39m")
+}
+hbar <- function() "\u2015"
+hbars <- function(n) if (n > 0) paste(rep(hbar(), n), collapse = "") else ""
+hspace <- function() " "
+hspaces <- function(n) if (n > 0) paste(rep(hspace(), n), collapse = "") else ""
+pb_build_msg <- function(action, pct_cmp, barwidth, secs = 0, action_pad = 1, secs_pad = 1) {
+  if (secs > 129599) {
+    secs <- sprintf("%3dd", as.integer(secs / 86400))
+  } else if (secs > 3599) {
+    secs <- sprintf("%3dh", as.integer(secs / 3600))
+  } else if (secs > 90) {
+    secs <- sprintf("%3dm", as.integer(secs / 60))
+  } else if (secs > 0) {
+    secs <- sprintf("%3ds", as.integer(secs))
+  } else {
+    secs <- ""
+  }
+  bar_cmp <- as.integer(pct_cmp * barwidth)
+  bar_rem <- barwidth - bar_cmp
+  paste0(
+    pmint(action), hspaces(action_pad),
+    ppink(hbars(bar_cmp)),
+    pgray(hbars(bar_rem)),
+    hspaces(secs_pad), ppurp(secs)
+  )
+}
 
 progress_bar <- function(its, fmt = "Working", width = NULL) {
     if (is.null(width)) {
-        width <- min(c(80, getOption("width", 80)), na.rm = TRUE)
+        width <- min(c(75, getOption("width", 75)), na.rm = TRUE)
     }
     .pb <- new.env()
     assign("i", 0L, envir = .pb)
     assign("N", its, envir = .pb)
-    fmt <- paste0("..", fmt, "..", paste(rep(" ", max(c(1, 15 - nchar(fmt)))),
-      collapse = ""), "[")
+    fmt <- paste0("..", fmt, "..", hspaces(max(c(1, 14 - nchar(fmt)))))
     mwidth <- width - nchar(fmt)
+    bar_width <- width - nchar(fmt) - 4L
     assign("width", width, envir = .pb)
-    pb_int <- mwidth / its
+    pb_int <- bar_width / its
     assign("pb_int", pb_int, envir = .pb)
     assign("pb_start_time", Sys.time() - 1, envir = .pb)
     list(
@@ -43,13 +71,14 @@ progress_bar <- function(its, fmt = "Working", width = NULL) {
             pb_secs_rmn <- as.integer(pb_secs_tot - s)
 
             ## build message, evaluate expression, print time remaining
-            msg <- paste0(fmt,
-              paste(rep("=", ceiling(pb_int * i)), collapse = ""))
-            width <- get("width", envir = .pb)
+            bar_width <- get("bar_width", envir = .pb)
+            msg <- pb_build_msg(fmt, pct_cmp = i / N, barwidth = bar_width, secs = pb_secs_rmn)
+            x <- eval(expr)
+
             if (i >= N) {
-                x <- pb_tick(expr, msg = msg, width = width)
+                cat(msg)
             } else {
-                x <- pb_tick(expr, msg = msg, secs = pb_secs_rmn, width = width)
+                cat(msg)
                 pb_clear_line(0)
             }
             x
@@ -57,39 +86,13 @@ progress_bar <- function(its, fmt = "Working", width = NULL) {
     )
 }
 
-pb_tick <- function(expr, msg = "=", secs = NULL, width = getOption("width")) {
-    x <- eval(expr)
-    if (!is.null(secs)) {
-        if (secs <= 120) {
-            secsfmt <- sprintf("%4ds", secs)
-        } else {
-            secsfmt <- springf("%4dm", ceiling(secs / 60))
-        }
-        sprmn <- width - nchar(msg) + 1L
-        if (sprmn > 0) {
-            secsfmt <- paste0(paste(rep(" ", sprmn), collapse = ""), secsfmt)
-        }
-        msg <- paste0(msg, ppurp(secsfmt))
-    }
-    m <- regexpr("\\[?\\=+\\]?", msg)
-    regmatches(msg, m) <- ppink(regmatches(msg, m))
-    m <- regexpr("\\S+", msg)
-    regmatches(msg, m) <- pmint(regmatches(msg, m))
-    cat(msg)
-    x
-}
 pb_clear_line <- function(width = getOption("width")) {
-    sp <- if (width > 0) {
-        rep(" ", width)
-    } else {
-        ""
-    }
+    sp <- hspaces(width)
     str <- paste0(c("\r", sp), collapse = "")
     cat(.makeMessage(str))
 }
 
 pb_end <- function(width = getOption("width")) {
-    cat(ppink("]"))
     str <- "\n"
     cat(.makeMessage(str))
 }
